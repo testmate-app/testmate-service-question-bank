7730054532d096cf96f562fa6a18457a65ec8a47
fikihfirmansyah <fikihfirmansyah43@gmail.com>
2024-11-02 03:08:36 +0700

    Initial commit

---
+++ src/main/java/id/or/mate/test/service_question_bank/TestMateQuestionBankServiceApplication.java
@@ -0,0 +1,13 @@
+package id.or.mate.test.service_question_bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMateQuestionBankServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestMateQuestionBankServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/or/mate/test/service_question_bank/TestMateQuestionBankServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.or.mate.test.service_question_bank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMateQuestionBankServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
